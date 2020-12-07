0413f1bc8b5da4ed6048947113b9f9d20bff63c7
Jaehwi_So <sjh9708@gmail.com>
2020-12-07 15:57:26 +0900

    C# 프로젝트의 기본 구조

---
+++ 01_Start/01_Start/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;   //기본적인 자료형, 숫자, 텍스트 등을 이용하기 위한 System 라이브러리 참조
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Start //클래스들을 충돌없이 편리하게 관리, 사용하기 위한 네임스페이스. 클래스는 네임스페이스 안에서 정의하는 것이 일반적이다.
+{
+    class Program   //Program이라는 클래스.
+    {
+        static void Main(string[] args) //메인 메서드
+        {
+            Console.WriteLine("Hello World");   //콘솔 출력
+        }
+    }
+}
