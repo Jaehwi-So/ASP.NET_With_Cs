3b477419725204554730ee0e865f9e9073dc2762
Jaehwi_So <sjh9708@gmail.com>
2020-12-09 13:43:55 +0900

    Static 메서드, 속성과 클래스

---
+++ 18_Static/18_Static/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Static
+{
+    public class StaticTestClass
+    {
+        //정적(스태틱) 필드 및 속성 : 클래스 인스턴스를 생성할 때 마다 메모리에 새로 생성되는 것이 아닌 처음으로 클래스가 사용될 때 초기화되어 계속 동일한 메모리를 사용한다.
+        protected static int name;
+        public static string Name { get; set;}
+        //인스턴스 메서드
+        public void no_static()
+        {
+            Console.WriteLine("no static method");
+        }
+
+        //정적 (스태틱) 메서드
+        //메서드 내부에서 클래스의 인스턴스 객체 멤버를 참조할 수 없다.
+        public static void method_static()
+        {
+            Console.WriteLine("static method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticTestClass static_test = new StaticTestClass();
+            //인스턴스 메서드 호출
+            static_test.no_static();
+            //static_test.method_static(); 인스턴스 객체로부터 호출될 수 없다.
+
+            //스태틱 메서드 호출
+            StaticTestClass.method_static();
+
+            //스태틱 속성, 필드 사용
+            StaticTestClass.Name = "kim";
+
+            //스태틱 클래스 사용
+            StaticClass.hello();
+        }
+    }
+}
---
+++ 18_Static/18_Static/StaticClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Static
+{
+    //static 클래스
+    //모든 클래스 멤버가 static, public 생성자를 가질 수 없다.(객체를 생성할 수 없음). 대신 static 생성자를 가질 수 있다.
+    //static 생성자는 static 필드들을 초기화 할 때 사용된다.
+    public static class StaticClass
+    {
+        private static int number;
+        static StaticClass()    //static 생성자에는 매개변수가 없어야 한다.
+        {
+            number = 10;
+        }
+
+        public static void hello()
+        {
+            Console.WriteLine("Hello " + number);
+        }
+    }
+}
