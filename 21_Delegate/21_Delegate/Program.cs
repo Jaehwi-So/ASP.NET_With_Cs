0ac482b1439e11aef13d989b41b0955f7ebf5e8a
Jaehwi_So <sjh9708@gmail.com>
2020-12-09 16:03:08 +0900

    Delegate : 델리게이트 정의를 통해 메서드를 인자로 전달하기

---
+++ 21_Delegate/21_Delegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* # Delegate
+ * 메서드를 다른 메서드의 인자로 전달할 수 있도록 하기 위해서 만들어졌다.
+ * 
+ */
+namespace _21_Delegate
+{
+    class Program
+    {
+        //델리게이트 정의하기.
+        //메서드 원형과 입력 파라미터 타입과 갯수, 리턴 타입이 동일하다면 해당 메서드는 해당 델리게이트에서 사용될 수 있다.
+        //메서드 파라미터와 리턴 타입에 대한 정의를 한 후 동일한 파라미터와 리턴 타입을 가진 메서드를 서로 호환하여 불러쓰는 것이 가능하다.
+        delegate int MyDelegate(int num, int num2);
+        void print(int num, int num2, MyDelegate method)
+        {
+            int result = method.Invoke(num, num2);  //Invoke() : 전달된 delegate로부터 실제 메서드 호출
+            Console.WriteLine(result);
+        }
+        public int plus(int num, int num2)
+        {
+            return num + num2;
+        }
+
+        public int minus(int num, int num2)
+        {
+            return num - num2;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            MyDelegate operate = new MyDelegate(p.plus);  //델리게이트 객체 생성. 
+            p.print(10, 20, operate); //델리게이트를 다른 메서드로 전달. 델리게이트 객체를 인수로 넣는다.
+
+            operate = p.minus;  //이와 같이 delegate 인스턴스를 표현 가능하다.
+            p.print(10, 20, operate);
+
+            //Delegate 예제
+            SortClass sort = new SortClass();
+            int[] array = { 7, 5, 15, 94, 32, 45, 77, 79, 5, 24, 12, 11, 8, 26 };
+            sort.test(array);
+        }
+    }
+}
---
+++ 21_Delegate/21_Delegate/SortClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Delegate
+{
+    public class SortClass
+    {
+        // 델리게이트 CompareDelegate 선언
+        public delegate int CompareDelegate(int i1, int i2);
+
+        //정렬
+        public void sort(int[] arr, CompareDelegate comp)
+        {
+            if (arr.Length < 2) return;
+            Console.WriteLine("함수 Prototype: " + comp.Method);  //델리게이트 프로토타입 확인
+
+            int is_change;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    is_change = comp(arr[i], arr[j]); //오름차순, 내림차순 비교
+                    if (is_change == -1)
+                    {
+                        // 교환
+                        int tmp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = tmp;
+                    }
+                }
+            }
+            display(arr);
+        }
+        public void display(int[] arr)
+        {
+            foreach (var i in arr) Console.Write(i + " ");
+            Console.WriteLine();
+        }
+        int AscendingCompare(int i1, int i2)
+        {
+            if (i1 == i2) return 0;
+            return (i2 - i1) > 0 ? 1 : -1;
+        }
+        int DescendingCompare(int i1, int i2)
+        {
+            if (i1 == i2) return 0;
+            return (i1 - i2) > 0 ? 1 : -1;
+        }
+        public void test(int[] array)
+        {
+            CompareDelegate ascendComp = AscendingCompare;
+            CompareDelegate descendComp = DescendingCompare;
+            sort(array, ascendComp);
+            sort(array, descendComp);
+        }
+    }
+}
