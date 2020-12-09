16e7f1dd01cdf5e06ee474fb56364b540a8f5052
Jaehwi_So <sjh9708@gmail.com>
2020-12-09 11:11:06 +0900

    예외 처리 try-catch-finally

---
+++ 15_Exception/15_Exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = { 1, 2, 3 };
+                arr[2] = 4;
+                Console.Write(arr[2]);
+                arr[3] = 5;
+                Console.Write(arr[3]);
+            }
+            catch (Exception e){
+                throw new Exception("error", e);
+            }
+            finally
+            {
+                Console.Write("end");
+            }
+        }
+    }
+}
